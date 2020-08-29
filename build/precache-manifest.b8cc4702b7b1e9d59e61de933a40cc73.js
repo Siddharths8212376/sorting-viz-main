768138cc54a26adeadcb96432868e925f4e8ebf9
Siddharths8212376 <siddharth8212376@gmail.com>
2020-08-29 15:19:32 +0530

    added the frontend build

---
+++ build/precache-manifest.b8cc4702b7b1e9d59e61de933a40cc73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0bd23da83953554116894471031a0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b757ca8deaa1b38455a3",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f06e3bbdac63379e365f",
+    "url": "/static/css/main.e0057438.chunk.css"
+  },
+  {
+    "revision": "b757ca8deaa1b38455a3",
+    "url": "/static/js/2.0b3711e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b3711e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f06e3bbdac63379e365f",
+    "url": "/static/js/main.6727d0e3.chunk.js"
+  },
+  {
+    "revision": "e7ab647c260e99affaf0",
+    "url": "/static/js/runtime-main.d3d27f49.js"
+  }
+]);
